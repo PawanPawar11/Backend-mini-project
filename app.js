27d708ab07faefbc29654d772d77712fd4a545c2
PawanPawar11 <pawanpawar4369@gmail.com>
2024-07-28 13:17:45 +0530

    fifth commit

--- app.js
+++ app.js
@@ -8,30 +8,33 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const path = require("path");
-const multer = require("multer");
+const multerconfig = require("./config/multerconfig");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/images/uploads");
-  },
-  filename: function (req, file, cb) {
-    crypto.randomBytes(12, function (err, bytes) {
-      const fn = bytes.toString("hex") + path.extname(file.originalname);
-      cb(null, fn);
-    });
-  },
-});
-
-const upload = multer({ storage: storage });
 
 app.get("/", function (req, res) {
   res.render("index");
 });
+
+app.get("/profile/upload", (req, res) => {
+  res.render("profileupload");
+});
+
+app.post(
+  "/upload",
+  isLoggedIn,
+  multerconfig.single("pfp"),
+  async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email });
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
+  }
+);
 
 app.get("/login", (req, res) => {
   res.render("login");
@@ -42,14 +45,6 @@ app.get("/profile", isLoggedIn, async (req, res) => {
     .findOne({ email: req.user.email })
     .populate("posts");
   res.render("profile", { user });
-});
-
-app.get("/test", (req, res) => {
-  res.render("test");
-});
-
-app.post("/upload", upload.single("pfp"), (req, res) => {
-  console.log(req.file);
 });
 
 app.get("/like/:id", isLoggedIn, async (req, res) => {
